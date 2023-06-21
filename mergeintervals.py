12e76054298ededc864b699a487545d8396b627a
vijayareddy2002 <83461890+vijayareddy2002@users.noreply.github.com>
2023-06-21 21:15:55 +0530

    Update mergeintervals.py
    Given an array of intervals where intervals[i] = [starti, endi], merge all overlapping intervals, and return an array of the non-overlapping intervals that cover all the intervals in the input.

--- mergeintervals.py
+++ mergeintervals.py
@@ -1,20 +1,24 @@
-'''Given an array of intervals where intervals[i] = [starti, endi], merge all overlapping intervals, and return an array of the non-overlapping intervals that cover all the intervals in the input.
-
-Example 1:
-
-Input: intervals = [[1,3],[2,6],[8,10],[15,18]]
-Output: [[1,6],[8,10],[15,18]]
-Explanation: Since intervals [1,3] and [2,6] overlap, merge them into [1,6].'''
-#----question----
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        if(len(intervals)==0 or len(intervals)==1):
-                return intervals
-        intervals.sort(key=lambda x:x[0])
-        res=[intervals[0]]
-        for interval in intervals[1:]:
-            if(interval[0]<=res[-1][1]):
-                res[-1][1]=max(interval[1],res[-1][1])
+        stack=[]
+        #sort according to first element
+        z=sorted(intervals, key=lambda x: x[0])
+        print(z)
+        stack.append(z[0])
+        a=stack[0][0]
+        b=stack[0][1]
+        for i in range(1,len(z)):
+            c=z[i][0]
+            d=z[i][1]
+            if(c<=b and d<=b):
+                stack.pop()
+                stack.append([a,b])
+            elif(c<=b and d>=b):
+                stack.pop()
+                stack.append([a,d])
             else:
-                res.append(interval)
-        return res
+                stack.append([c,d])
+            a=stack[-1][0]
+            b=stack[-1][1]
+        return stack
+        
